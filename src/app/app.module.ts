5a6f2772d47ca168127303507837d9e1aae4fdf6
akshayr20 <akshay.rajput5394@gmail.com>
2019-02-16 12:55:29 +0530

    moved picture and content in stories section, created a new layout in stories component for same

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,9 +9,9 @@ import { FeaturesComponent } from './features/features.component';
 import { HomesComponent } from './homes/homes.component';
 import { GalleryComponent } from './gallery/gallery.component';
 import { FooterComponent } from './footer/footer.component';
-import { StoryPictureComponent } from './story-picture/story-picture.component';
-import { StoryContentComponent } from './story-content/story-content.component';
 import { StoriesComponent } from './stories/stories.component';
+import { StoryPictureComponent } from './stories/story-picture/story-picture.component';
+import { StoryContentComponent } from './stories/story-content/story-content.component';
 
 @NgModule({
   declarations: [
