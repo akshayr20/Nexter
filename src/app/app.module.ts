31829a2b53d44a716e254b98d51821a621b4ce3c
akshayr20 <akshay.rajput5394@gmail.com>
2019-02-16 10:56:51 +0530

    created a grid layout for the website, added essential components which will be required for building the app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { HeaderComponent } from './header/header.component';
+import { RealtorsComponent } from './realtors/realtors.component';
+import { FeaturesComponent } from './features/features.component';
+import { HomesComponent } from './homes/homes.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { FooterComponent } from './footer/footer.component';
+import { StoryPictureComponent } from './story-picture/story-picture.component';
+import { StoryContentComponent } from './story-content/story-content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    HeaderComponent,
+    RealtorsComponent,
+    FeaturesComponent,
+    StoryPictureComponent,
+    StoryContentComponent,
+    HomesComponent,
+    GalleryComponent,
+    FooterComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
